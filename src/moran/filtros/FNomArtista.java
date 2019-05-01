6a2a6d9ec06ac5eaedd45f828eff8382985254cd
ElielCatauro <ecatauronardella@alumnos.exa.unicen.edu.ar>
2019-05-01 15:40:44 -0300

    filtros abstractos , sin interface para  Asignar la clase filtro a cualquira de los filtros

--- src/moran/filtros/FNomArtista.java
+++ src/moran/filtros/FNomArtista.java
@@ -3,5 +3,5 @@ package moran.filtros;
 import moran.Pista;
 
 public class FNomArtista extends Filtro{
-    public abstract Boolean compara (Pista cancion);
+    public  Boolean compara (Pista cancion);
 }
