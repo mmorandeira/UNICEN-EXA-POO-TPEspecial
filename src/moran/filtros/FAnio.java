d8b3ac631586e7803e905460c61649450d3601dc
ElielCatauro <ecatauronardella@alumnos.exa.unicen.edu.ar>
2019-05-01 15:49:01 -0300

    comparador para los años

--- src/moran/filtros/FAnio.java
+++ src/moran/filtros/FAnio.java
@@ -3,5 +3,17 @@ package moran.filtros;
 import moran.Pista;
 
 public class FAnio extends Filtro{
-    public Boolean compara (Pista cancion);
+
+private int anio;
+
+        public FAnio(int anio)
+        {
+            this.anio=anio;
+        }
+
+    public Boolean compara (Pista cancion)
+    {
+        return (cancion.getAnio() < anio);
+    }
+
 }
