b7a8a808f7d171b501338d90d0957df05a7f12dc
ElielCatauro <ecatauronardella@alumnos.exa.unicen.edu.ar>
2019-05-01 15:42:26 -0300

    filtros abstractos , sin interface para  Asignar la clase filtro a cualquira de los filtros

--- src/moran/filtros/FNOT.java
+++ src/moran/filtros/FNOT.java
@@ -3,5 +3,10 @@ package moran.filtros;
 import moran.Pista;
 
 public class FNOT extends Filtro{
-    public Boolean compara (Pista cancion);
+
+    private Filtro primero;
+
+    public Boolean compara (Pista cancion){
+        return !primero.compara(cancion)
+    }
 }
