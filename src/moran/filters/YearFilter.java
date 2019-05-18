7b0357e9bef7e4af1fdb84d671f4183b6d4d4368
Mattii97 <morandeiramatias@gmail.com>
2019-05-18 16:42:14 -0300

    Se renombro comp a comparator.

--- src/moran/filters/YearFilter.java
+++ src/moran/filters/YearFilter.java
@@ -4,15 +4,15 @@ import moran.structures.Track;
 
 public class YearFilter implements Filter {
     private int year;
-    private Comparator comp;
+    private Comparator comparator;
 
     public YearFilter(int year, Comparator comp) {
         this.year = year;
-        this.comp = comp;
+        this.comparator = comp;
     }
 
     @Override
     public boolean accept(Track track) {
-        return this.comp.compare(track.getYear(), this.year);
+        return this.comparator.compare(track.getYear(), this.year);
     }
 }
