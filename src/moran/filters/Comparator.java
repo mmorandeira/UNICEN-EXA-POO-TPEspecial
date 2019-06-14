c2158fac66c892e7d1ea3b00e7cbf16d6dc73525
Mattii97 <Morandeiramatias@gmail.com>
2019-06-14 08:46:00 -0300

    Se cambio == por equals.

--- src/moran/filters/Comparator.java
+++ src/moran/filters/Comparator.java
@@ -9,9 +9,9 @@ public class Comparator {
 
     public boolean compare(Comparable c1, Comparable c2) {
         int res = c1.compareTo(c2);
-        if (mode == ComparatorMode.EQUAL && res == 0) return true;
-        if (mode == ComparatorMode.GREATER && res > 0) return true;
-        if (mode == ComparatorMode.LESS && res < 0) return true;
+        if (mode.equals(ComparatorMode.EQUAL) && res == 0) return true;
+        if (mode.equals(ComparatorMode.GREATER) && res > 0) return true;
+        if (mode.equals(ComparatorMode.LESS) && res < 0) return true;
         return false;
     }
 }
